8547428bf136c2655e244e72b5c72d4df7e6eb30
kaave <abe@framelunch.jp>
2018-05-10 23:25:58 +0900

    Add thwfwa & base system

--- entry.py
+++ entry.py
@@ -1,5 +1,9 @@
+import os
 import json
-from src import awwwards, css_design_awards, css_winner
+import requests
+from datetime import datetime
+from src import awwwards, css_design_awards, css_winner, the_fwa
+
 # {
 #     "attachments": [
 #         {
@@ -16,6 +20,63 @@ from src import awwwards, css_design_awards, css_winner
 #     ]
 # }
 
+
+def main():
+    if not os.path.exists('./.env'):
+        print('ERROR: Not found .env!')
+        return
+
+    hashes_filename = os.getenv('HASHES_FILE', '')
+
+    hashes = {
+        'awwwards': '',
+        'css_design_awards': '',
+        'css_winner': '',
+        'the_fwa': '',
+    }
+
+    if os.path.exists(hashes_filename):
+        with open(hashes_filename, 'r') as f:
+            hashes = json.loads(f.read())
+
+    results = {
+        'awwwards': awwwards.get(hashes['awwwards']),
+        'css_design_awards':
+        css_design_awards.get(hashes['css_design_awards']),
+        'css_winner': css_winner.get(hashes['css_winner']),
+        'the_fwa': the_fwa.get(hashes['the_fwa']),
+    }
+
+    attachments = []
+    for key, result in results.items():
+        if not bool(result):
+            continue
+
+        hashes[key] = result['current_hash']
+        attachments.append({
+            'ts': int(datetime.now().timestamp()),
+            'color': result['color'],
+            'author_name': result['author_name'],
+            'author_link': result['author_link'],
+            'title': result['title'],
+            'title_link': result['title_link'],
+            'text': result['text'],
+            'image_url': result['image_url'],
+        })
+
+    if len(attachments) == 0:
+        return
+
+    slack_url = os.getenv('SLACK_WEB_HOOK_URL', '')
+    requests.post(
+        slack_url,
+        data=json.dumps({
+            'text': '<!here> Hurry and Check it!',
+            'attachments': attachments
+        }))
+    with open(hashes_filename, 'w') as f:
+        f.write(json.dumps(hashes))
+
+
 if __name__ == '__main__':
-    results = [awwwards.get(), css_design_awards.get(), css_winner.get()]
-    print(json.dumps(results))
+    main()
---
+++ src/the_fwa.py
@@ -0,0 +1,48 @@
+import re
+import requests
+from bs4 import BeautifulSoup
+import hashlib
+import json
+import subprocess
+
+
+def get(prev_hash=''):
+    try:
+        response = requests.get('https://thefwa.com/awards')
+    except Exception as error:
+        print(error)
+        return
+
+    current_hash = hashlib.md5(response.text.encode()).hexdigest()
+    if prev_hash == current_hash:
+        return
+
+    soup = BeautifulSoup(response.text, 'html5lib')
+    js_code = soup.find('script').text.strip()
+    remove_comment_code = re.sub('<!--.+-->', '', js_code)
+    node_response = subprocess.run(
+        [
+            'node', '-e',
+            f'{remove_comment_code} console.log(JSON.stringify(konfig))'
+        ],
+        stdout=subprocess.PIPE)
+    info = json.loads(node_response.stdout)
+
+    return {
+        'current_hash':
+        current_hash,
+        'color':
+        '#ffca00',
+        'author_name':
+        'The FWA',
+        'author_link':
+        'https://thefwa.com',
+        'title':
+        info['caseMenu'][0]['item']['title'],
+        'title_link':
+        f'https://thefwa.com/cases/{info["caseMenu"][0]["item"]["slug"]}',
+        'text':
+        '(UNKNOWN)',
+        'image_url':
+        f'https://thefwa.com{info["caseMenu"][0]["item"]["slide1"]["538"]["span10"]}'
+    }
