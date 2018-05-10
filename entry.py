ee2e4a613ad59b730a762a1f8846a00ec95d85d5
kaave <abe@framelunch.jp>
2018-05-10 23:26:28 +0900

    Remove memo comment

--- entry.py
+++ entry.py
@@ -4,22 +4,6 @@ import requests
 from datetime import datetime
 from src import awwwards, css_design_awards, css_winner, the_fwa
 
-# {
-#     "attachments": [
-#         {
-#             "color": "#f1efea",
-#             "pretext": "@here Hurry and Check it!",
-#             "author_name": "CSS Design Awards",
-#             "author_link": "https://www.cssdesignawards.com",
-#             "title": "Slack API Documentation",
-#             "title_link": "https://api.slack.com/",
-#             "text": "Optional text that appears within the attachment",
-#             "image_url": "https://www.cssdesignawards.com/cdasites/2018/201804/20180424023416.jpg",
-#             "ts": 1525874410
-#         }
-#     ]
-# }
-
 
 def main():
     if not os.path.exists('./.env'):
